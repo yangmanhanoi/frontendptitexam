6569675475f43023acf04494814cf6d6473a54c4
Duong Tuan Nam <namduongtuan27@gmail.com>
2024-05-08 10:11:21 +0700

    base layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import { useState, useEffect } from 'react'
+import Login from './LogIn/Login';
+import Layout from './Layout/Layout.jsx';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import useAxiosFetch from './hooks/useAxiosFetch'
+import LoginSignup from './LoginSignUp/LoginSignUp'
+import Home from './Home/Home.jsx';
 function App() {
+  const [isLogInSuccess, setLogInSuccess] = useState(false)
+  const [isLogIn, setIsLogin] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path='' element ={<Home></Home>}/>
+            <Route path='/home' element ={<Home></Home>}/>
+          </Route>
+          <Route path='/login'
+          element={<Login/>}/>
+          <Route path='/register'
+          element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './hooks/AuthProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <App></App>
+
+  </AuthProvider> 
 );
 
 // If you want to start measuring performance in your app, pass a function
