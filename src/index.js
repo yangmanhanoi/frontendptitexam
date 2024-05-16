5fc60c1366ff991d6186712e028114ef66dc816f
Duong Tuan Nam <namduongtuan27@gmail.com>
2024-05-16 23:11:23 +0700

    home, quizz, exam, result page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './hooks/AuthProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import '@mui/icons-material'
+import {BrowserRouter, Route, useNavigate} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
